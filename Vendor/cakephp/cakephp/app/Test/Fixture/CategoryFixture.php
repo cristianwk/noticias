33cdd192c485ee9ee287eb8518d452a6cd26d321
cristianwk <cristian@consultoriawk.com>
2022-08-25 16:26:05 -0300

    subindo projeto
    primeiro commit

---
+++ Vendor/cakephp/cakephp/app/Test/Fixture/CategoryFixture.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Category Fixture
+ */
+class CategoryFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 1, 'unsigned' => false, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'charset' => 'utf8mb4'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8mb4', 'collate' => 'utf8mb4_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'name' => 'Lorem ipsum dolor sit amet',
+			'created' => '2022-08-25 20:50:48',
+			'modified' => '2022-08-25 20:50:48'
+		),
+	);
+
+}
